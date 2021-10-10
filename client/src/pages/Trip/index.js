b34780ce4eb0bcc46bcb908db77eaa0a5b3aba85
“ELOBO5” <elicialobo@hotmail.com>
2021-10-10 12:47:11 +0100

    add conditional rendering

--- client/src/pages/Trip/index.js
+++ client/src/pages/Trip/index.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+import "./style.css";
 
 const Trip = () => {
-  return <div></div>;
+  const location = useLocation();
+  let header;
+  let tripcard;
+
+  if (location.pathname === "/creatortrip") {
+    header = "Creator Trip";
+    tripcard = "Trip Card Info Here";
+  } else if (location.pathname === "/yourtrip") {
+    header = "Your Trip";
+    tripcard = "Trip Card Info with Reactions Here";
+  }
+
+  return (
+    <div className="trip-container">
+      <h1>{header}</h1>
+      <div className="card">
+        <div className="card-body">{tripcard}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Trip;
