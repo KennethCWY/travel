11e121888a331ec5fdb2519b28103685cf697fe7
“ELOBO5” <elicialobo@hotmail.com>
2021-10-11 10:18:24 +0100

    update search form

--- client/src/components/SearchBar/SearchBar.test.js
+++ client/src/components/SearchBar/SearchBar.test.js
@@ -1,16 +1,16 @@
-import { default as SearchBar } from ".";
-import { screen, render } from "@testing-library/react";
+import { default as SearchBar } from '.';
+import { screen, render } from '@testing-library/react';
 
-describe("SearchBar", () => {
-  let getResultMock;
+describe('SearchBar', () => {
+    let getResultMock;
 
-  beforeEach(() => {
-    getResultMock = jest.fn();
-    render(<SearchBar />);
-  });
+    beforeEach(() => {
+        getResultMock = jest.fn();
+        render(<SearchBar />);
+    });
 
-  test("it renders a form", () => {
-    let form = screen.getByRole("form");
-    expect(form).toBeInTheDocument();
-  });
+    test('it renders a form', () => {
+        let form = screen.getByRole('form');
+        expect(form).toBeInTheDocument();
+    });
 });
