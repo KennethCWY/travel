49a76302b23cfbaeefa7ca4407ee47e9ff782af5
“ELOBO5” <elicialobo@hotmail.com>
2021-10-12 11:03:06 +0100

    add private route

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,39 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import { NotFound } from "./components";
-import { Header, Footer } from "./layout";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { NotFound, PrivateRoute } from './components';
+import { Header, Footer } from './layout';
 import {
-  Explore,
-  Accommodation,
-  Experiences,
-  Flights,
-  LandingPage,
-  Login,
-  Register,
-  Dashboard,
-  Trip
-} from "./pages";
+    Explore,
+    Accommodation,
+    Experiences,
+    Flights,
+    LandingPage,
+    Login,
+    Register,
+    Dashboard,
+    Trip
+} from './pages';
 
 const App = () => {
-  return (
-    <>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={LandingPage} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/explore" component={Explore} />
-        <Route path="/flights" component={Flights} />
-        <Route path="/accommodation" component={Accommodation} />
-        <Route path="/experiences" component={Experiences} />
-        <Route path="/creatortrip" component={Trip} />
-        <Route path="/yourtrip" component={Trip} />
-        <NotFound />
-      </Switch>
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <Switch>
+                <PrivateRoute exact path="/" component={LandingPage} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+                <PrivateRoute path="/dashboard" component={Dashboard} />
+                <PrivateRoute path="/explore" component={Explore} />
+                <PrivateRoute path="/flights" component={Flights} />
+                <PrivateRoute path="/accommodation" component={Accommodation} />
+                <PrivateRoute path="/experiences" component={Experiences} />
+                <PrivateRoute path="/creatortrip" component={Trip} />
+                <PrivateRoute path="/yourtrip" component={Trip} />
+                <NotFound />
+            </Switch>
+            <Footer />
+        </>
+    );
 };
 
 export default App;
