991e38a21850fec2f1c3c5a6b1bbd0a4d4267b9a
“ELOBO5” <elicialobo@hotmail.com>
2021-10-12 11:19:31 +0100

    add logout button to navbar

--- client/src/layout/Header/index.js
+++ client/src/layout/Header/index.js
@@ -1,50 +1,53 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import "./style.css";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './style.css';
+import { Logout } from '../../components';
 
 const Header = () => {
-  return (
-    <nav className="navbar static-top navbar-expand-lg navbar-dark bg-dark">
-      <NavLink exact to="/" className="navbar-brand">
-        Oyster Card
-      </NavLink>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
+    return (
+        <nav className="navbar static-top navbar-expand-lg navbar-dark bg-dark">
+            <NavLink exact to="/" className="navbar-brand">
+                Oyster Card
+            </NavLink>
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
 
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-          <NavLink to="/flights" className=" nav-item nav-link">
-            Flights
-          </NavLink>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <NavLink to="/flights" className=" nav-item nav-link">
+                        Flights
+                    </NavLink>
 
-          <NavLink to="/accommodation" className="nav-item nav-link">
-            Accommodation
-          </NavLink>
+                    <NavLink to="/accommodation" className="nav-item nav-link">
+                        Accommodation
+                    </NavLink>
 
-          <NavLink to="/experiences" className="nav-item nav-link">
-            Experiences
-          </NavLink>
+                    <NavLink to="/experiences" className="nav-item nav-link">
+                        Experiences
+                    </NavLink>
 
-          <NavLink to="/login" className="nav-item nav-link">
-            Login
-          </NavLink>
+                    <NavLink to="/login" className="nav-item nav-link">
+                        Login
+                    </NavLink>
 
-          <NavLink to="/register" className="nav-item nav-link">
-            Register
-          </NavLink>
-        </div>
-      </div>
-    </nav>
-  );
+                    <NavLink to="/register" className="nav-item nav-link">
+                        Register
+                    </NavLink>
+
+                    <Logout />
+                </div>
+            </div>
+        </nav>
+    );
 };
 
 export default Header;
