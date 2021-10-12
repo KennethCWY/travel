22d431181dc9c7ea7353408e535f388126b36502
Gorazd Sumkovski <gorazd@getfutureproof.co.uk>
2021-10-12 15:32:42 +0100

    Flight fetching is now done in flight page rather than fligh card

--- client/src/components/FlightCard/index.js
+++ client/src/components/FlightCard/index.js
@@ -1,65 +1,21 @@
 import React from "react"
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import axios from 'axios'
 
 import "./style.css";
-import { addFlight, randomiseArray } from '../../redux/actions.js'
 
-function FlightCard() {
-    
-    let flights = [];
-    const dispatch = useDispatch();
-    const cityName = useSelector(store => store.city);
-    const countryCode = useSelector(store => store.countryCode);
 
-    useEffect(() => {
-        async function fetchData() {
-            console.log(cityName);
-            console.log(countryCode);
-
-            const cityData = await axios.get('https://api.npoint.io/5f5a6588530da581be26');
-            const cityArr = cityData.data.filter(city => city.name === cityName && city.country_code === countryCode);
-            const cityCode = cityArr[0].code;
-
-            const airportData = await axios.get('https://api.npoint.io/cf06c4767429ca337264');
-            const airports =  airportData.data.filter(airport => airport.city_code === cityCode && airport.iata_type === 'airport' && airport.flightable === true);
-            const airportCodes = airports.map(airport => airport.code);
-            console.log(airports);
-            console.log(airportCodes);
-
-            for (let aCode of airportCodes) {
-                
-                const params = {
-                    access_key: '5f8bb73bf843d5c5441e420597322ae3',
-                    dep_iata: aCode
-                }
-                
-                try {
-                    const flightsData = await axios.get('http://api.aviationstack.com/v1/flights', {params});
-                    const scheduledFlights = flightsData.data.data.filter(flight => flight.flight_status === 'scheduled');
-                    flights.push(...scheduledFlights);
-                } catch (err) {
-                    console.error(err);
-                }
-            }
-            dispatch(addFlight(randomiseArray(flights)));
-        }
-        fetchData();
-    }, []);
-
+function FlightCard({flight}) {
 
     return (
         <div className='grid-container'>
-            <div>London</div>
+            <div>{flight.depCity}</div>
             <div> <p id='flight-time'>6h</p> <span id='arrow'>&#10230;</span> </div>
-            <div>Dubai</div>
-            <div>10:40 28/10/2021</div>
-            <div>British Airways</div>
-            <div>14:20 15/11/20201</div>
-            <div>Heathrow Airport</div>
-            <div>Price: £99</div>
-            <div>Dubai International Airport</div>
+            <div>{flight.destCity}</div>
+            <div>{flight.depDateTime}</div>
+            <div>{flight.airline}</div>
+            <div>{flight.destDateTime}</div>
+            <div>{flight.depAirport}</div>
+            <div>Price:</div>
+            <div>{flight.destAirport}</div>
         </div>
     )
 }
--- client/src/pages/Flights/index.js
+++ client/src/pages/Flights/index.js
@@ -1,8 +1,74 @@
 import React from "react";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import axios from 'axios'
+
 import { FlightCard } from "../../components";
+import { addFlight, randomiseArray } from '../../redux/actions.js'
+
 
 const Flights = () => {
-  return <FlightCard />
+
+  let flights = [];
+  const dispatch = useDispatch();
+  const cityName = useSelector(state => state.city);
+  const countryCode = useSelector(state => state.countryCode);
+
+  // const REACT_APP_AVIATIONSTACK_API_KEY = process.env.REACT_APP_AVIATIONSTACK_API_KEY;
+  // console.log(REACT_APP_AVIATIONSTACK_API_KEY);
+
+  useEffect(() => {
+      async function fetchData() {
+
+          const cityData = await axios.get('https://api.npoint.io/5f5a6588530da581be26');
+          const cityArr = cityData.data.filter(city => city.name === cityName && city.country_code === countryCode);
+          const cityCode = cityArr[0].code;
+
+          const airportData = await axios.get('https://api.npoint.io/cf06c4767429ca337264');
+          const airports =  airportData.data.filter(airport => airport.city_code === cityCode && airport.iata_type === 'airport' && airport.flightable === true);
+          const airportCodes = airports.map(airport => airport.code);
+
+          for (let aCode of airportCodes) {
+              
+              const params = {
+                  access_key: '8d3a4746bce14c7460d5bcbe07f678f9',
+                  dep_iata: aCode
+              }
+              
+              try {
+                  const flightsData = await axios.get('http://api.aviationstack.com/v1/flights', {params});
+                  const scheduledFlights = flightsData.data.data.filter(flight => flight.flight_status === 'scheduled');
+                  flights.push(...scheduledFlights);
+              } catch (err) {
+                  console.error(err);
+              }
+          }
+          dispatch(addFlight(randomiseArray(flights)));
+          console.log(flights);
+      }
+      fetchData();
+  }, []);
+
+  const numOfCards = 3;
+  let flightCards = [];
+
+  for (let i = 0; i < numOfCards; i++) {
+    
+    let flight = flights[i];
+
+    let flightInfo = {
+      depCity: cityName,
+      depDateTime: "flight.departure.estimated.split('T').join('  ')",
+      depAirport: 'flight.departure.airport',
+      airline: 'flight.airline.name',
+      destCity: 'assume user also inputs this, so get from redux store',
+      destDateTime: "flight.arrival.estimated.split('T').join('  ')",
+      destAirport: 'flight.arrival.airport'
+    }
+    flightCards.push(<FlightCard flight={flightInfo} key={i} />)
+  }
+
+  return <div>{flightCards}</div>
 };
 
 export default Flights;
