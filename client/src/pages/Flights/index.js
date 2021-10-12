6475659e97f76ef72b4124f8c12a3f08a6049d58
Gorazd Sumkovski <gorazd@getfutureproof.co.uk>
2021-10-12 17:28:00 +0100

    Can get flights, difficulty render the flight cards

--- client/src/pages/Flights/index.js
+++ client/src/pages/Flights/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import axios from 'axios'
 
@@ -8,16 +8,23 @@ import { addFlight, randomiseArray } from '../../redux/actions.js'
 
 
 const Flights = () => {
-
+  
+  const [x, setX] = useState(null);
+  
+  const numOfCards = 3;
   let flights = [];
+  let flightCards = [];
+  console.log('thwehwef', flightCards);
   const dispatch = useDispatch();
-  const cityName = useSelector(state => state.city);
-  const countryCode = useSelector(state => state.countryCode);
+  const cityName = useSelector(state => state.destinationCity);
+  const countryCode = useSelector(state => state.destinationCountryCode);
+  console.log(cityName);
+  console.log(countryCode);
 
   // const REACT_APP_AVIATIONSTACK_API_KEY = process.env.REACT_APP_AVIATIONSTACK_API_KEY;
   // console.log(REACT_APP_AVIATIONSTACK_API_KEY);
 
-  useEffect(() => {
+  
       async function fetchData() {
 
           const cityData = await axios.get('https://api.npoint.io/5f5a6588530da581be26');
@@ -32,7 +39,7 @@ const Flights = () => {
               
               const params = {
                   access_key: '8d3a4746bce14c7460d5bcbe07f678f9',
-                  dep_iata: aCode
+                  arr_iata: aCode
               }
               
               try {
@@ -43,32 +50,42 @@ const Flights = () => {
                   console.error(err);
               }
           }
+
+          for (let i = 0; i < numOfCards; i++) {
+            
+            let flight = flights[i];
+            console.log(flight);
+
+            let flightInfo = {
+              depCity: 'assume user also inputs this, so get from redux store',
+              depDateTime: "flight.departure.estimated.split('T').join('  ')",
+              depAirport: 'flight.departure.airport',
+              airline: 'flight.airline.name',
+              destCity: cityName,
+              destDateTime: "flight.arrival.estimated.split('T').join('  ')",
+              destAirport: 'flight.arrival.airport'
+            }
+            flightCards.push(<FlightCard flight={flightInfo} key={i} />)
+          }
           dispatch(addFlight(randomiseArray(flights)));
-          console.log(flights);
+          console.log('qwerqwr', flightCards);
+          setX(true);
+          return
       }
-      fetchData();
-  }, []);
+      
+      
+      useEffect(() => {
+        fetchData();
+      },[])
 
-  const numOfCards = 3;
-  let flightCards = [];
-
-  for (let i = 0; i < numOfCards; i++) {
-    
-    let flight = flights[i];
-
-    let flightInfo = {
-      depCity: cityName,
-      depDateTime: "flight.departure.estimated.split('T').join('  ')",
-      depAirport: 'flight.departure.airport',
-      airline: 'flight.airline.name',
-      destCity: 'assume user also inputs this, so get from redux store',
-      destDateTime: "flight.arrival.estimated.split('T').join('  ')",
-      destAirport: 'flight.arrival.airport'
-    }
-    flightCards.push(<FlightCard flight={flightInfo} key={i} />)
-  }
-
-  return <div>{flightCards}</div>
+ 
+  
+  
+  
+  return x
+    ? <div>{flightCards}</div>
+    : <div>bionerwu</div>
+  
 };
 
 export default Flights;
