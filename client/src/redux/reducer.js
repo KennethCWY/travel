08cc82723084721068029a20bafa1ec09b8840fc
Tobi Kuyoro <tobikuyo@gmail.com>
2021-10-12 10:35:06 +0100

    Make changes to the reducer

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -3,9 +3,13 @@ import {
     ADD_FLIGHT,
     ADD_HOTEL,
     ADD_RESTAURANT,
+    UPDATE_BOUNDS,
+    UPDATE_CITY,
+    UPDATE_COORDINATES,
+    UPDATE_COUNTRY,
+    UPDATE_COUNTRY_CODE,
     UPDATE_DESTINATION,
-    UPDATE_TRIP_DETAILS,
-    UPDATE_TRIP_ID
+    UPDATE_TRIP_DETAILS
 } from './constants';
 
 const initialState = {
@@ -19,6 +23,11 @@ const initialState = {
     restaurants: [],
     attractions: [],
 
+    // Related to Flights
+    city: '',
+    country: '',
+    countryCode: '',
+
     // Related to Places API
     bounds: {},
     coordinates: {}
@@ -26,15 +35,14 @@ const initialState = {
 
 const tripReducer = (state = initialState, { payload, type }) => {
     switch (type) {
-        case UPDATE_TRIP_ID:
-            return { ...state, tripId: payload };
         case UPDATE_DESTINATION:
             return { ...state, destination: payload };
         case UPDATE_TRIP_DETAILS:
             return {
                 ...state,
+                tripId: payload.tripId,
                 departure: payload.departure,
-                destination: !state.destination ? payload.destination : state.destination,
+                destination: payload.destination,
                 departureDate: payload.departureDate,
                 returnDate: payload.returnDate
             };
@@ -46,6 +54,16 @@ const tripReducer = (state = initialState, { payload, type }) => {
             return { ...state, restaurants: [...state.restaurants, payload] };
         case ADD_ATTRACTION:
             return { ...state, attractions: [...state.attractions, payload] };
+        case UPDATE_BOUNDS:
+            return { ...state, bounds: payload };
+        case UPDATE_COORDINATES:
+            return { ...state, coordinates: payload };
+        case UPDATE_CITY:
+            return { ...state, city: payload };
+        case UPDATE_COUNTRY:
+            return { ...state, country: payload };
+        case UPDATE_COUNTRY_CODE:
+            return { ...state, countryCode: payload };
         default:
             return state;
     }
