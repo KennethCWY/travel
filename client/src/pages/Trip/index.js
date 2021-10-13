76f2e78c34dee367f2d7ab77dd7c76006958dc18
Tobi Kuyoro <tobikuyo@gmail.com>
2021-10-13 16:47:07 +0100

    Create restaurand card component and render it in trip card

--- client/src/pages/Trip/index.js
+++ client/src/pages/Trip/index.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import Moment from 'react-moment';
 import { useParams } from 'react-router-dom';
 import { HotelCard } from '../../components';
+import RestaurantCard from '../../components/RestaurantCard';
 import './style.css';
 
 const Trip = () => {
@@ -40,9 +41,18 @@ const Trip = () => {
             </div>
 
             {/* Hotels */}
+            {hotels.length ? <h2>Hotels</h2> : null}
             <div className="flex-container">
                 {hotels.map(hotel => (
                     <HotelCard hotel={hotel} key={hotel.id} />
+                ))}
+            </div>
+
+            {/* Restaurants */}
+            {restaurants.length ? <h2>Restaurants</h2> : null}
+            <div className="flex-container">
+                {restaurants.map(restaurant => (
+                    <RestaurantCard restaurant={restaurant} key={restaurant.id} />
                 ))}
             </div>
         </div>
