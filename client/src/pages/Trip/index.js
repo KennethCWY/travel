bd902d1cc36966a462d320034287f6924c8e2343
Tobi Kuyoro <tobikuyo@gmail.com>
2021-10-14 09:19:13 +0100

    Show reactions on hotel card

--- client/src/pages/Trip/index.js
+++ client/src/pages/Trip/index.js
@@ -12,6 +12,18 @@ const Trip = () => {
     const [hotels, setHotels] = useState([]);
     const [restaurants, setRestaurants] = useState([]);
     const [attractions, setAttractions] = useState([]);
+    const [userReactions, setUserReactions] = useState(null);
+    
+        useEffect(() => {
+            const getCurrentUserReactions = async () => {
+                const currentUserId =  Number(localStorage.getItem('user_id'));
+                const { data } = await axios.get('http://localhost:8000/api/experience_reactions/');
+                const userReactions = await data?.filter(reaction => reaction.user === currentUserId);
+                setUserReactions(userReactions);
+            };
+
+            getCurrentUserReactions();
+        }, []);
 
     useEffect(() => {
         const getTripInfo = async () => {
@@ -33,7 +45,7 @@ const Trip = () => {
         <div className="trip-container">
             <h1>{trip.destination}</h1>
 
-            {/* Trip Details */}
+            {/* Trip Dates */}
             <div className="flex-row">
                 <Moment format="Do MMM YYYY">{trip.departureDate}</Moment>
                 <span> - </span>
@@ -44,7 +56,11 @@ const Trip = () => {
             {hotels.length ? <h2>Hotels</h2> : null}
             <div className="flex-container">
                 {hotels.map(hotel => (
-                    <HotelCard hotel={hotel} key={hotel.id} />
+                    <HotelCard 
+                        hotel={hotel} 
+                        userReactions={userReactions} 
+                        key={hotel.id}
+                    />
                 ))}
             </div>
 
@@ -52,7 +68,11 @@ const Trip = () => {
             {restaurants.length ? <h2>Restaurants</h2> : null}
             <div className="flex-container">
                 {restaurants.map(restaurant => (
-                    <RestaurantCard restaurant={restaurant} key={restaurant.id} />
+                    <RestaurantCard 
+                        restaurant={restaurant}
+                        userReactions={userReactions} 
+                        key={restaurant.id}
+                    />
                 ))}
             </div>
         </div>
