5ff26ff85da664e02adb5d972f930296dcde9d05
KennethCWY <yeung.kenneth.c.w@gmail.com>
2021-10-14 18:32:58 +0100

    remove frontend

--- client/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
-import App from "./App";
-
-test("renders navbar links", () => {
-  render(<App />, { wrapper: MemoryRouter });
-  const linkElement = screen.getByText(/Oyster card/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- client/src/layout/Footer/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-const Footer = () => (
-  <span id="footer">&copy; Tobi, Kenneth & Elicia 2021</span>
-);
-
-export default Footer;
